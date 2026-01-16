52774e584752705978a2c72d9d4517822a9d17a1
lukekenny <lckenny@outlook.com>
2026-01-17 02:49:51 +1100

    Add use_ssl configuration for Qdrant vector store

--- openmemory/api/app/utils/memory.py
+++ openmemory/api/app/utils/memory.py
@@ -152,7 +152,8 @@ def get_default_memory_config():
         vector_store_provider = "qdrant"
         vector_store_config.update({
             "host": os.environ.get('QDRANT_HOST'),
-            "port": int(os.environ.get('QDRANT_PORT'))
+            "port": int(os.environ.get('QDRANT_PORT')),
+            "use_ssl": "true"
         })
     elif os.environ.get('WEAVIATE_CLUSTER_URL') or (os.environ.get('WEAVIATE_HOST') and os.environ.get('WEAVIATE_PORT')):
         vector_store_provider = "weaviate"
